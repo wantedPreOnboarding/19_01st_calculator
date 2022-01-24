591521cb4fdfd7829cc8c8ed47c1af5f18c9f0b3
hyun <vi2920va@gmail.com>
2022-01-24 18:25:03 +0900

    Modify - 라우터 연결

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Pages/App';
+import Routes from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
