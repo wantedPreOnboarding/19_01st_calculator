b6f8218fcc69deb6a2e339ab55ba8f2760049c86
yeongjong310 <tohero21@naver.com>
2022-01-25 17:51:36 +0900

    Add - 환률 정보를 객체 형석으로 반환하는 비동기 함수 구현
    issue num. #2

--- src/api/core.js
+++ src/api/core.js
@@ -7,3 +7,12 @@ const instance = axios.create({
 
 const currencyApi = key =>
   instance.get(`/live?access_key=${key}&currencies=${countries.join(',')}&source=USD&format=1`);
+
+export const getCurrencies = async () => {
+  try {
+    const { data } = await currencyApi(process.env.REACT_APP_CURRENCYLAYER_API);
+    return data.quotes;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
