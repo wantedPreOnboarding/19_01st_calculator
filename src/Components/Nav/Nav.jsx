e6e1f2482c6553e019cd0158a7b79e9f31699166
yeongjong310 <tohero21@naver.com>
2022-01-26 03:09:30 +0900

    Add - Nav 스타일 수정

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.scss';
 
-const Nav = () => {
+const Nav = props => {
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <ul className="navbar--ul">
         <li>
-          <Link to="/exchangeCalc">계산기 1</Link>
+          <Link to="/">계산기 1</Link>
         </li>
         <li>
           <Link to="/multiExchangeCalc">계산기 2</Link>
