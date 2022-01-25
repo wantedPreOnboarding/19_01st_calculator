ab8ad1820ff31676d4970dd79249d8875df96d2a
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-01-25 15:42:10 +0900

    Add - calc2  라우터 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main';
+import ExchangeCalc2 from './Pages/ExchangeCalc2';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Main} />
+        <Route exact path="/calc2" component={ExchangeCalc2} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 };
 
 export default Routes;
