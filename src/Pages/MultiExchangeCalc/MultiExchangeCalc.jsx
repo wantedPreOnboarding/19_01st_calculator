4774496af0d40ba5705dbbab1702ad3238e45f78
yeongjong310 <tohero21@naver.com>
2022-01-25 22:56:46 +0900

    Modify - 탭 박스의 버튼 값과 셀렉트 박스의 값이 동일할 경우에 생기는 버그 해결
    - 상세설명:
    SelectBox의 상태가 바뀔 때 탭 박스의 값과 같다면 선택된 탭이 보이지 않는 문제 해결

--- src/Pages/MultiExchangeCalc/MultiExchangeCalc.jsx
+++ src/Pages/MultiExchangeCalc/MultiExchangeCalc.jsx
@@ -3,8 +3,15 @@ import './MultiExchangeCalc.scss';
 import { multiCountries } from '../../api/constants';
 
 const MultiExchangeCalc = () => {
-  const [selectedCountry, setSelectedCountry] = useState('USD');
-  const [selectedMenuNum, setSeltectedMenuNum] = useState(0);
+  const [sendCountry, setSendCountry] = useState('USD');
+  const [recvCountry, setRecvCountry] = useState('CAD');
+
+  const sendCountryHandler = selectedCountry => {
+    if (selectedCountry === recvCountry) {
+      setRecvCountry(multiCountries.filter(country => country !== selectedCountry)[0]);
+    }
+    setSendCountry(selectedCountry);
+  };
 
   return (
     <div className="ex-calc2">
@@ -13,7 +20,7 @@ const MultiExchangeCalc = () => {
         <select
           name="currencies"
           className="ex-calc2-head__select"
-          onChange={e => setSelectedCountry(e.target.value)}
+          onChange={e => sendCountryHandler(e.target.value)}
         >
           {multiCountries.map(country => {
             return (
@@ -27,14 +34,14 @@ const MultiExchangeCalc = () => {
       <div className="ex-calc2-body__box">
         <ul className="ex-calc2-body__tab">
           {multiCountries
-            .filter(country => country !== selectedCountry)
-            .map((country, index) => {
+            .filter(country => country !== sendCountry)
+            .map(country => {
               return (
                 <li
-                  className={`ex-calc2-body__tab--menu ${index === selectedMenuNum && 'active'}`}
+                  className={`ex-calc2-body__tab--menu ${country === recvCountry && 'active'}`}
                   key={country}
                 >
-                  <button value={country} onClick={() => setSeltectedMenuNum(index)}>
+                  <button value={country} onClick={() => setRecvCountry(country)}>
                     {country}
                   </button>
                 </li>
