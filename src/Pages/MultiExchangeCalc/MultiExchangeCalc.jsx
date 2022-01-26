6090f0f1fa60817e37cc563e139ea7381c6f918c
pctmustang <85476908+leechoiswim1@users.noreply.github.com>
2022-01-27 01:42:31 +0900

    Refactor 멀티계산기 form/tab 컴포넌트 분리

--- src/Pages/MultiExchangeCalc/MultiExchangeCalc.jsx
+++ src/Pages/MultiExchangeCalc/MultiExchangeCalc.jsx
@@ -5,9 +5,10 @@ import './MultiExchangeCalc.scss';
 import { multiCountries } from '../../api/constants';
 import { getCurrencies } from '../../api/core';
 /* == Custom - utils */
-import commaNumber from '../../utils/commaNumber';
 import currencyExchanger from '../../utils/currencyExchanger';
 import formattedDate from '../../utils/formattedDate';
+import MultiCalcForm from '../../Components/MultiCalc/MultiCalcForm';
+import MultiCalcTab from '../../Components/MultiCalc/MultiCalcTab';
 
 const MultiExchangeCalc = () => {
   const [sendCountry, setSendCountry] = useState('USD');
@@ -15,12 +16,6 @@ const MultiExchangeCalc = () => {
   const [valueInput, setValueInput] = useState('');
   const [{ currencies, timestamp }, setCurrencies] = useState({});
   const [resultValue, setResultValue] = useState(0);
-
-  //currencyExchanger에서 받은 결과를 상태로 연동
-  const changeResult = (sendCountry, recvCountry) => {
-    const result = currencyExchanger(sendCountry, recvCountry, valueInput, currencies);
-    setResultValue(result);
-  };
 
   //사용자가 input을 입력 후 전송하면 결과창을 변경하는 함수
   const submitHandler = e => {
@@ -50,6 +45,12 @@ const MultiExchangeCalc = () => {
     setValueInput(filteredValue <= 0 ? '' : filteredValue > 1000 ? 1000 : filteredValue);
   };
 
+  //currencyExchanger에서 받은 결과를 상태로 연동
+  const changeResult = (sendCountry, recvCountry) => {
+    const result = currencyExchanger(sendCountry, recvCountry, valueInput, currencies);
+    setResultValue(result);
+  };
+
   useEffect(() => {
     (async () => {
       const currencies = await getCurrencies();
@@ -59,49 +60,14 @@ const MultiExchangeCalc = () => {
 
   return (
     <div className="ex-calc2">
-      <form
-        className="ex-calc2-head__form"
-        onSubmit={e => {
-          submitHandler(e);
-        }}
-      >
-        <input
-          type="text"
-          className="ex-calc2-head__input"
-          value={commaNumber(valueInput)}
-          onChange={e => inputHandler(e.target.value)}
-        />
-        <select
-          name="currencies"
-          className="ex-calc2-head__select"
-          onChange={e => sendCountryHandler(e.target.value)}
-        >
-          {multiCountries.map(country => {
-            return (
-              <option key={country} value={country}>
-                {country}
-              </option>
-            );
-          })}
-        </select>
-      </form>
+      <MultiCalcForm
+        valueInput={valueInput}
+        submitHandler={submitHandler}
+        inputHandler={inputHandler}
+        sendCountryHandler={sendCountryHandler}
+      />
       <div className="ex-calc2-body__box">
-        <ul className="ex-calc2-body__tab">
-          {multiCountries
-            .filter(country => country !== sendCountry)
-            .map(country => {
-              return (
-                <li
-                  className={`ex-calc2-body__tab--menu ${country === recvCountry && 'active'}`}
-                  key={country}
-                >
-                  <button value={country} onClick={() => tabHandler(country)}>
-                    {country}
-                  </button>
-                </li>
-              );
-            })}
-        </ul>
+        <MultiCalcTab sendCountry={sendCountry} recvCountry={recvCountry} tabHandler={tabHandler} />
         <div className="ex-calc2-body__result">
           <span className="ex-calc2-body__result--main-text">
             {recvCountry} : {resultValue}
